6c49cfc12f298de712b4aa0c18df366c8bfe3fad
Tim <evanstde@gmail.com>
2025-08-28 16:54:54 +0100

    Neaten declaration of fixes
    Split up the definition of fixes into individual variables rather than
    a single huge slice literal

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -28,92 +28,85 @@ func main() {
 		log.Fatalf("Couldn't get release information for khaldun.net client: %s\n", err.Error())
 	}
 
-	// Ngl this is fucking ugly as sin
-	// like the idea is ok, but doing individual append calls and stuff
-	// would probably be a lot less heinous
+	kg_fix := Fix{
+		description: "Kohan Gold",
+		check: []string{kg_target.filename},
+		download: []TargetInfo{kg_target},
+	}
+
+	launcher_fix := Fix{
+		description: "Kohan Launcher",
+		check: []string{"KohanLauncher.exe"},
+		remove: nil,
+		download: []TargetInfo{{
+			downloadurl: "https://github.com/Kohan-Citadel/kohangold-KG-/releases/download/v0.9.6/KohanLauncher.exe",
+			filename: "KohanLauncher.exe",
+		}},
+	}
+
+	khaldun_client_fix := Fix{
+		description: "khaldun.net client",
+		check: []string{"dinput.dll"},
+		download: []TargetInfo{khaldun_target},
+		unpack: []string{khaldun_target.filename},
+		cleanup: []string{khaldun_target.filename},
+	}
+
+	append_to_cnc_ini := func() {
+		throbber.Delay()
+		log.Println("cnc-ddraw - Adding Kohan settings to ddraw.ini")
+		to_append := strings.Join([]string{
+			"; Kohan: Ahriman's Gift",
+			"[_ag]",
+			"windowed=false",
+			"fullscreen = true",
+			"maintainas=true",
+		}, "\n")
+		filehandle, err := os.OpenFile("ddraw.ini", os.O_APPEND | os.O_WRONLY, 0644)
+		if err != nil {
+			throbber.Delay()
+			log.Printf("cnc-ddraw - Error: Could not open 'ddraw.ini' for writing: %s\n",
+				err.Error())
+			return
+		}
+		defer filehandle.Close()
+
+		if _, err := filehandle.WriteString(to_append); err != nil {
+			throbber.Delay()
+			log.Printf("cnc-ddraw - Error: could not append to 'ddraw.ini': %s",
+				err.Error())
+		}
+	}
+
+	ddraw_fix := Fix{
+		description: "cnc-ddraw",
+		check: []string{
+			"ddraw.dll",
+			"ddraw.ini",
+			"cnc-ddraw config.exe",
+			"Shaders/",
+		},
+		download: []TargetInfo{ddraw_target},
+		remove: []string{
+			"D3D8.dll",
+			"D3D9.dll",
+			"D3DImm.dll",
+			"DDraw.dll",
+			"dgVoodoo.conf",
+			"dgVoodooCpl.exe",
+			"libwine.dll",
+			"wine3d.dll",
+		},
+		unpack: []string{ddraw_target.filename},
+		cleanup: []string{ddraw_target.filename},
+		extra: append_to_cnc_ini,
+	}
+
 	fixes := []Fix{
-		{
-			description: "Kohan Gold",
-			check: []string{kg_target.filename},
-			download: []TargetInfo{kg_target},
-			remove: nil,
-			unpack: nil,
-			rename: nil,
-			cleanup: nil,
-			extra: nil,
-		},
-
-		{
-			description: "Kohan Launcher",
-			check: []string{"KohanLauncher.exe"},
-			remove: nil,
-			download: []TargetInfo{{
-				downloadurl: "https://github.com/Kohan-Citadel/kohangold-KG-/releases/download/v0.9.6/KohanLauncher.exe",
-				filename: "KohanLauncher.exe",
-			}},
-			unpack: nil,
-			rename: nil,
-			cleanup: nil,
-			extra: nil,
-		},
-
-		{
-			description: "khaldun.net client",
-			check: []string{"dinput.dll"},
-			download: []TargetInfo{khaldun_target},
-			remove: nil,
-			unpack: []string{khaldun_target.filename},
-			rename: nil,
-			cleanup: []string{khaldun_target.filename},
-			extra: nil,
-		},
-
-		{
-			description: "cnc-ddraw",
-			check: []string{
-				"ddraw.dll",
-				"ddraw.ini",
-				"cnc-ddraw config.exe",
-				"Shaders/",
-			},
-			download: []TargetInfo{ddraw_target},
-			remove: []string{
-				"D3D8.dll",
-				"D3D9.dll",
-				"D3DImm.dll",
-				"DDraw.dll",
-				"dgVoodoo.conf",
-				"dgVoodooCpl.exe",
-				"libwine.dll",
-				"wine3d.dll",
-			},
-			unpack: []string{ddraw_target.filename},
-			rename: nil,
-			cleanup: []string{ddraw_target.filename},
-			extra: func() {
-				to_append := strings.Join([]string{
-					"; Kohan: Ahriman's Gift",
-					"[_ag]",
-					"windowed=false",
-					"fullscreen = true",
-					"maintainas=true",
-				}, "\n")
-				filehandle, err := os.OpenFile("ddraw.ini", os.O_APPEND | os.O_WRONLY, 0666)
-				if err != nil {
-					throbber.Delay()
-					log.Printf("cnc-ddraw - Error: Could not open 'ddraw.ini' for writing: %s\n",
-						err.Error())
-					return
-				}
-				defer filehandle.Close()
-
-				if _, err := filehandle.WriteString(to_append); err != nil {
-					throbber.Delay()
-					log.Printf("cnc-ddraw - Error: could not append to 'ddraw.ini': %s",
-						err.Error())
-				}
-			},
-		},
+		kg_fix,
+		launcher_fix,
+		khaldun_client_fix,
+		ddraw_fix,
 	}
 
 	var waitgroup sync.WaitGroup
@@ -123,5 +116,6 @@ func main() {
 	}
 	waitgroup.Wait()
 	throbber.Stop()
-	log.Println("Done!")
+	log.Println("Done! Press Enter to quit")
+	fmt.Scanln()
 }
